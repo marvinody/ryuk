01325e8a37c15747d666ca4f69766c0c041ffa5b
Manuel Bugallo <marvinody@gmail.com>
2021-01-10 17:12:56 -0500

    logger + run script

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,5 @@
 const { createLogger, format, transports } = require('winston')
+const path = require('path')
 
 const logger = createLogger({
   level: process.env.LOG_LEVEL || 'info',
@@ -15,6 +16,13 @@ const logger = createLogger({
   transports: [
     new transports.Console({
       timestamp: true,
+    }),
+    new transports.File({
+      filename: path.join(__dirname, '..', 'logs', 'combined.log'),
+      maxFiles: 10,
+      maxsize: 500 * 1024, // 500kb per log file
+      zippedArchive: true,
+      tailable: true,
     }),
   ]
 });

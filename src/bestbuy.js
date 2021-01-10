c42c3212118a13a9742f62b29f1c5fc15fe12148
Manuel Bugallo <marvinody@gmail.com>
2021-01-09 20:06:41 -0500

    testing if new cookie header works on vps

--- src/bestbuy.js
+++ src/bestbuy.js
@@ -20,6 +20,7 @@ const options = {
     "Upgrade-Insecure-Requests": "1",
     "Cache-Control": "max-age=0",
     "TE": "Trailers",
+    "Cookie": "UID=47bebf67-c772-4624-8f0e-bda45291b838; pst2=478|N; physical_dma=501; customerZipCode=11377|N; oid=1773878008; vt=cf38a210-5184-11eb-83dd-12e56de9e2ab; _abck=5D35BD88BD3E4F258E4EDA66C9497F27~-1~YAAQNJUzuARwCJ12AQAAH7zw4AV/YEGreArubYLddaXbmCDGm3L03S2KldWuog8H4TxilxrTags9EkPlVPyytduo56cORVtUYj6CS+iwtecR6NmJH9BE1jTbYk4BxdEAN9vElBBouCkxW5zddQMeH0+8ai9w2ZZ6P+5XVcqs2FQ13LmQavd5YkXA33eDnTYwLtqJaKSZvxU/ke1dfHAU+W2opXGXpNYPo6SR0Z8YbxNj+ctz7uRtOmN9CRFJRaFWdFE6Dr9cncrUwmb6YyG99B5rv9YtyEu/V+kGxx5Ywv75RPQ74MB+gSZXfw==~-1~-1~-1; bm_sz=A274554B2CCE0C567ED11ED249675B89~YAAQNJUzuANwCJ12AQAAH7zw4ApJc4DowmOzZ3mWS2b2qSpl9RnvT/tSlN59feV6ggCZUjgCXXkExCBfRsDnNMgS9NC0dujkkarbmnPVnNtpw6EvOFCG34xB7m8mVYljpMqByI/vrHmEOLusktwv1NzGjJMm2nSTvbWtCnKcG4SchEkq+7HqaA14gJW975yLog==; bby_rdp=l; CTT=6ee4138fb450441cfafbffc5b13a0241; SID=5b2ce497-14ec-447e-b918-7b00fffe8416"
   }
 }
 
@@ -34,11 +35,6 @@ const makeUrl = ({ sku }) => {
     .replace(/\{\{sku\}\}/g, sku)
 }
 
-
-// x('', '').then((value) => {
-//   console.log({ value })
-//   a.a.aa
-// })
 
 const ADD_TO_CART_SELECTOR = '.fulfillment-add-to-cart-button button'
 

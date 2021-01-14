7ccf0adf6b38b794fb1a17774bad0a04ba1b12a5
Manuel Bugallo <marvinody@gmail.com>
2021-01-14 00:52:44 -0500

    log envs cause issue

--- src/sitrep/index.js
+++ src/sitrep/index.js
@@ -4,22 +4,22 @@ const bestbuy = require('../bestbuy')
 const discord = require('../discord')
 
 const sku = "6430161";
+console.log(JSON.stringify(process.env, null, 2))
+// (async () => {
+//   try {
 
-(async () => {
-  try {
+//     const result = await bestbuy({ sku });
 
-    const result = await bestbuy({ sku });
+//     if (result.sku !== sku) {
+//       throw new Error(`SKU mismatch: Expected "${result.sku}" to equal "${sku}"`);
+//     }
 
-    if (result.sku !== sku) {
-      throw new Error(`SKU mismatch: Expected "${result.sku}" to equal "${sku}"`);
-    }
-
-    if (result.status !== "Sold Out") {
-      throw new Error(`Status mismatch: Expected "${result.status}" to be "Sold Out"`)
-    }
-    await discord.webhook("SNAFU")
-  } catch (err) {
-    console.error(err)
-    await discord.webhook(err.message)
-  }
-})()
+//     if (result.status !== "Sold Out") {
+//       throw new Error(`Status mismatch: Expected "${result.status}" to be "Sold Out"`)
+//     }
+//     await discord.webhook("SNAFU")
+//   } catch (err) {
+//     console.error(err)
+//     await discord.webhook(err.message)
+//   }
+// })()
